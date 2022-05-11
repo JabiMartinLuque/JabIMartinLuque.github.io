a749d03c4e318ec373ae6bbd4cd14963e46e9356
Francisco Javier Martín Luque <100721820+JabiMartinLuque@users.noreply.github.com>
2022-05-11 17:58:08 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 import {DBManager} from './DBManager.js';
 
+const loginUsername = document.getElementById("loginUsername");
+const loginPassword = document.getElementById("loginPassword");
+
+const signupUsername = document.getElementById("signupUsername");
+const signupPassword = document.getElementById("signupPassword");
+const confirmPassword = document.getElementById("confirmPassword");
+
+
 function setFormMessage(formElement, type, message) {
     const messageElement = formElement.querySelector(".form__message");
 
@@ -18,9 +26,12 @@ function clearInputError(inputElement) {
     inputElement.parentElement.querySelector(".form__input-error-message").textContent = "";
 }
 
+
 document.addEventListener("DOMContentLoaded", () => {
     const loginForm = document.querySelector("#login");
     const createAccountForm = document.querySelector("#createAccount");
+    const db = new DBManager();
+    db.init();
 
     document.querySelector("#linkCreateAccount").addEventListener("click", e => {
         e.preventDefault();
@@ -36,39 +47,39 @@ document.addEventListener("DOMContentLoaded", () => {
 
     loginForm.addEventListener("submit", async e => {
         e.preventDefault();
-        const basedato = new DBManager();
-        basedato.init();
+        
         //const usuario = await basedato.loginUser(usuario, contraseña);
         //Prueba con usuario conocido en BD \/
-        const usuario = await basedato.loginUser("diablo", "mami");
+        const usuario = await db.loginUser(loginUsername.value, loginPassword.value);
         //console.log(await basedato.loginUser("diablo", "mami"));
         console.log(usuario);
-        if(usuario == 0)
+        if(usuario == 0 || usuario == -1)
         {
             setFormMessage(loginForm, "error", "Invalid username/password combination");
         }else
         {
             console.log("Hemos iniciado sesión en usuario: " + usuario.user + " con la contraseña: " + usuario.Password + " con un nivel de experiencia: " + usuario.EXP);
-            setFormMessage(loginForm, "success", "You loged in succesfully!")
-            window.location = "./main.html";
+            sessionStorage.setItem("name", loginUsername.value)
+            window.location.href = "./main.html";
         }
-
-        // LOGIN
-
     });
 
-    document.querySelectorAll(".form__input").forEach(inputElement => {
-        inputElement.addEventListener("blur", e => {
-            if (e.target.id === "signupUsername" && e.target.value.length > 0 && e.target.value.length < 5) {
-                setInputError(inputElement, "Username must be at least 5 characters in length");
+     createAccountForm.addEventListener("submit", async e => {
+        e.preventDefault()
+        
+        if (signupPassword.value!=confirmPassword.value){
+            setFormMessage(createAccountForm, "error", "The passwords doesn't match")
+        }else{
+            const isRegistered = await db.loginUser(signupUsername.value, "")
+            if(isRegistered==-1){
+                const registeredUser = await db.registerUser(signupUsername.value, signupPassword.value);
+                setFormMessage(loginForm, "success", "You signed up succesfully!")
+                loginForm.classList.remove("form--hidden");
+                createAccountForm.classList.add("form--hidden");
+            }else{
+                setFormMessage(createAccountForm, "error", "The user already exists!")
             }
-            else {
-                //REGISTER
-            }
-        });
-
-        inputElement.addEventListener("input", e => {
-            clearInputError(inputElement);
-        });
-    });
+        }
+        
+    }); 
 });
--- main.js
+++ main.js
@@ -1,84 +1,101 @@
+import {DBManager} from './DBManager.js';
+
 
 const Pinwi = document.getElementById("pinwi")
 const Name = document.getElementById("name")
 const Head = document.getElementById("head")
 const body = document.getElementById("body")
-const d1 = document.getElementById("d1")
-const d2 = document.getElementById("d2")
+const d = document.getElementById("d")
+const face = document.getElementById("face")
 const Ropa = document.getElementById("roro")
 const money = document.getElementById("money")
+const Lvl = document.getElementById("lvl")
+const Exp = document.getElementById("exp")
 
-var exp = 0
-var lvl = 0
+const db = new DBManager();
+db.init();
+
+
+var lvl= 0   
 var initWidth = Pinwi.clientWidth
 var cont= 0
+var user =sessionStorage.getItem("name")
 
-var mon = sessionStorage.getItem("m")
+var mon = await db.getCoins(user)
+var exp = await db.getExp(user)
+
 money.innerHTML= mon +"€"
 
+
 Pinwi.addEventListener("click", pinwiFunction) 
-d2.addEventListener("click", pinwiFunction) 
+face.addEventListener("click", pinwiFunction) 
 Ropa.addEventListener("click", dress) 
-money.addEventListener("click", hereComes) 
 
+    lvl=Math.trunc(exp/10)
+    exp=exp%10;
+    
 
-function hereComes(){
-    mon++
-    money.innerHTML=mon +"€"
+switch (lvl){
+    case 0: break
+    case 1: Pinwi.src="./skin/huevoRotoF.png"; break;
+    case 2: Pinwi.src = "./skin/pinwiBBF.png"; break;
+    default: Pinwi.src = "./skin/pinwiAdulF.png"; break;
 }
+
+Lvl.innerHTML = "LVL "+ lvl
+Exp.innerHTML = exp + " EXP"
+Name.innerHTML=user
+
 
 
 function dress(){
+
     if(cont==0){
         Head.src="./ropa/gorroF.png";
         body.src="./ropa/cadenaF.png";
-        d2.src="./ropa/gatoF.png";
+        d.src="./ropa/chanclasF.png";
+        face.src="./ropa/gafasF.png"
         cont++
-    }else{
+
+    }else if(cont==1){
         Head.src="./ropa/narutoF.png";
         body.src="./ropa/betisF.png";
-        d2.src="./ropa/amogusF.png";
-        cont--
+        d.src="./ropa/amogusF.png";
+        face.src="./ropa/vacio.png"
+        cont++
+
+    }else{
+        Head.src ="./ropa/marioF.png"
+        body.src="./ropa/pajaF.png"
+        d.src="./ropa/gatoF.png"
+        face.src="./ropa/vacio.png"
+        cont=cont-2
     }
 }
 
 
 function pinwiFunction () {
     // console.log(pinwiclicked)
-    let nameRect = Name.getBoundingClientRect()
-    let pinwiRect = Pinwi.getBoundingClientRect()
     exp++
-    let Exp = document.getElementById("exp")
-    let Lvl = document.getElementById("lvl")
     let currWidth = Pinwi.clientWidth
     console.log(currWidth)
-    if(exp==10){
-        exp-=10;
-        lvl++;
-    }
-    switch (lvl){
-        case 0: break
-        case 1: Pinwi.src="./skin/huevoRotoF.png"; break;
-        case 2: Pinwi.src = "./skin/pinwiBBF.png"; break;
-        default: Pinwi.src = "./skin/pinwiAdulF.png"; break;
-    }
 
 
-    Pinwi.style.width = (initWidth*1.03) + "px"
-    Head.style.width = (initWidth*1.03) + "px"
-    body.style.width = (initWidth*1.03) + "px"
-    d1.style.width = (initWidth*1.03) + "px"
-    d2.style.width = (initWidth*1.03) + "px"
+
+    Pinwi.style.width = (initWidth*1.05) + "px"
+    Head.style.width = (initWidth*1.05) + "px"
+    body.style.width = (initWidth*1.05) + "px"
+    d.style.width = (initWidth*1.05) + "px"
+    face.style.width = (initWidth*1.05) + "px"
     setTimeout(() => {  Pinwi.style.width = initWidth + "px"; }, 150);
     setTimeout(() => {  Head.style.width = initWidth + "px"; }, 150);
     setTimeout(() => {  body.style.width = initWidth + "px"; }, 150);
-    setTimeout(() => {  d1.style.width = initWidth + "px"; }, 150);
-    setTimeout(() => {  d2.style.width = initWidth + "px"; }, 150);
+    setTimeout(() => {  d.style.width = initWidth + "px"; }, 150);
+    setTimeout(() => {  face.style.width = initWidth + "px"; }, 150);
 
     console.log(nameRect.top)
-    Lvl.innerHTML = "LVL "+ lvl
-    Exp.innerHTML = exp + " EXP"
-
     }
+
+
 
 
